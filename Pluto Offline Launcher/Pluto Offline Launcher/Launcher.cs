6e597d15ce414f7bae487e032bc79ef7b57f6427
Thegan Govender <govenderthegan@gmail.com>
2020-01-27 01:19:21 +0200

    v1.0.5.1

--- Pluto Offline Launcher/Pluto Offline Launcher/Launcher.cs
+++ Pluto Offline Launcher/Pluto Offline Launcher/Launcher.cs
@@ -1,12 +1,10 @@
+using AutoUpdaterDotNET;
 using System;
-
 using System.Diagnostics;
-
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Windows.Forms;
-using AutoUpdaterDotNET;
-
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
@@ -16,11 +14,10 @@ namespace Pluto_Offline_Launcher
     public partial class Launcher : Form
     {
         //Variables
-        string gameloc = "";
-        string plutolauncherlink = "https://fastcache.plutonium.pw/r372.zip";
+        private string gameloc = "";
+
+        private string plutolauncherlink = "https://fastcache.plutonium.pw/r372.zip";
         private static string loc = Path.GetDirectoryName(Application.ExecutablePath) + "\\";
-
-
 
         //methods
         public void create_player_files()
@@ -29,11 +26,9 @@ namespace Pluto_Offline_Launcher
             if (!Directory.Exists(gameloc + "t6r\\player"))
             {
                 Directory.CreateDirectory(gameloc + "t6r\\player");
-               
             }
             if (!File.Exists(loc + "t6r\\player\\user.name"))
             {
-                
                 StreamWriter fileout = new StreamWriter(gameloc + "t6r\\player\\user.name");
 
                 fileout.Write("Player");
@@ -41,12 +36,10 @@ namespace Pluto_Offline_Launcher
             }
 
             //----------------------------------------------------------------------
-
         }
 
         public void create_system_files()
         {
-
             //---------------------Config Files-------------------------------------
             if (!File.Exists(loc + "Config\\path.config"))
             {
@@ -60,38 +53,34 @@ namespace Pluto_Offline_Launcher
                 nFile.Close();
             }
             //-----------------------------------------------------------------------
-
         }
-
 
         public void getusername()
         {
-                 try
-                     {
-                        string un = File.ReadAllText(gameloc + "t6r\\player\\user.name");
-                        if (!un.Equals(""))
-                         {
-                           txt_Username.Text = un;
-                         }
-                        else
-                         {
-                           MessageBox.Show("Username Not Set.", "Set Username.",
-                           MessageBoxButtons.OK, MessageBoxIcon.Error);
-                         }
-                     }
-                 catch
-                    {
-                        if(!gameloc.Equals("Locate Game Executable"))
-                         {
-                           create_player_files();
-                         }   
-                     }
+            try
+            {
+                string un = File.ReadAllText(gameloc + "t6r\\player\\user.name");
+                if (!un.Equals(""))
+                {
+                    txt_Username.Text = un;
+                }
+                else
+                {
+                    MessageBox.Show("Username Not Set.", "Set Username.",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+                if (!gameloc.Equals("Locate Game Executable"))
+                {
+                    create_player_files();
+                }
+            }
         }
 
         public void setusername()
         {
-           
-
             try
             {
                 if (!txt_Username.Text.Equals(""))
@@ -105,37 +94,27 @@ namespace Pluto_Offline_Launcher
                     MessageBox.Show("Username Cannot Be Blank.", "Enter a Username.",
     MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
-
             }
             catch
             {
                 MessageBox.Show("Username Not Set.", "Error Setting Username.",
    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
-
 
         public bool autolocate()
         {
             bool found = false;
-            if (File.Exists(loc+ "t6rmp.exe"))
-                {
+            if (File.Exists(loc + "t6rmp.exe"))
+            {
                 lbl_Location.Text = loc;
 
                 found = true;
-               
-                setpath(loc.Substring(0,loc.Length-1));
+
+                setpath(loc.Substring(0, loc.Length - 1));
             }
             return found;
-
-             
-
         }
-
-
-
 
         public void checkgame()
         {
@@ -147,7 +126,19 @@ namespace Pluto_Offline_Launcher
             {
                 btn_ZM.Enabled = true;
             }
-          
+        }
+
+        public void set_button_state(bool enable)
+        {
+            if (enable)
+            {
+                checkgame();
+            }
+            else
+            {
+                btn_MP.Enabled = false;
+                btn_ZM.Enabled = false;
+            }
         }
 
         public void Get_Latest_Version_Plutonium_Client()
@@ -170,45 +161,36 @@ namespace Pluto_Offline_Launcher
                         nFile.Close();
                     }
                 }
-
             }
             catch
             {
-
             }
-           
-           
         }
 
         public void updatelabel()
         {
             try
             {
-
                 string v = "R372-Default";
                 string un = File.ReadAllText(loc + "Config\\plutoversion.xml");
 
-                    string[] xmlconf = un.Split('<','>','=');
-                    v = xmlconf[2];
-                    plutolauncherlink = xmlconf[5];
-                    linkLabel1.Text = "Use with Latest Patch Only ("+v+" - Click to Download)";
+                string[] xmlconf = un.Split('<', '>', '=');
+                v = xmlconf[2];
+                plutolauncherlink = xmlconf[5];
+                linkLabel1.Text = "Use with Latest Patch Only (" + v + " - Click to Download)";
             }
             catch
             {
-              
             }
-         
         }
 
         public void getpath()
         {
-          
             try
             {
                 string path = File.ReadAllText(loc + "Config\\path.config");
                 if (!path.Equals("Locate Game Executable"))
                 {
-
                     lbl_Location.Text = path;
                     gameloc = path.Replace("\\", "\\\\");
                 }
@@ -221,15 +203,14 @@ namespace Pluto_Offline_Launcher
             {
                 MessageBox.Show("path.config not found.", "Error: File Not Found.",
 MessageBoxButtons.OK, MessageBoxIcon.Error);
-
             }
-
         }
+
         public void setpath(string fileloc)
         {
-            if(!lbl_Location.Text.Equals("Locate Game Executable"))
+            if (!lbl_Location.Text.Equals("Locate Game Executable"))
             {
-                File.WriteAllText(loc + "Config\\path.config", fileloc+"\\");
+                File.WriteAllText(loc + "Config\\path.config", fileloc + "\\");
                 getpath();
             }
             else
@@ -237,9 +218,8 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
                 MessageBox.Show("Game Directory Not Set.", "Locate Game Executable.",
 MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-           
-
         }
+
         public bool usernameExists()
         {
             bool e = false;
@@ -249,6 +229,30 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
                 e = true;
             }
             return e;
+        }
+
+
+        private void AutoUpdater_ApplicationExitEvent()
+        {
+            try
+            {
+                // Check if file exists with its full path    
+                if (File.Exists(Path.Combine(loc, "ZipExtractor.exe")))
+                {
+                    // If file found, delete it    
+                    File.Delete(Path.Combine(loc, "ZipExtractor.exe"));
+                    File.Delete(Path.Combine(loc, "ZipExtractor.log"));
+                    File.Delete(Path.Combine(loc, "Pluto Offline Launcher.exe"));
+                    Debug.WriteLine("File deleted.");
+                }
+
+            }
+            catch (IOException ioExp)
+            {
+                Debug.WriteLine(ioExp.Message);
+            }
+           
+            Application.Exit();
         }
 
         public Launcher()
@@ -273,7 +277,6 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
         {
             // Click on the link below to continue learning how to build a desktop app using WinForms!
             System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -283,15 +286,21 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
 
         private void Form1_Load(object sender, EventArgs e)
         {
-          //  AutoUpdater.DownloadPath = Environment.CurrentDirectory;
-         //   AutoUpdater.Start("https://github.com/CyferShepard/Pluto-Offline-Launcher/blob/master/vcheck/updater.xml");
-          // MessageBox.Show("Thanks!");
+            //  AutoUpdater.DownloadPath = Environment.CurrentDirectory;
+            //   AutoUpdater.Start("https://github.com/CyferShepard/Pluto-Offline-Launcher/blob/master/vcheck/updater.xml");
+            // MessageBox.Show("Thanks!");
         }
+
         public void update()
         {
             AutoUpdater.ReportErrors = false;
             AutoUpdater.DownloadPath = Environment.CurrentDirectory;
+            AutoUpdater.ApplicationExitEvent += AutoUpdater_ApplicationExitEvent;
+            AutoUpdater.ShowSkipButton = false;
+            AutoUpdater.RunUpdateAsAdmin = true;
             AutoUpdater.Start("https://raw.githubusercontent.com/CyferShepard/Pluto-Offline-Launcher/master/vcheck/updater.xml");
+
+
         }
 
         private void btn_minimize_Click(object sender, EventArgs e)
@@ -301,13 +310,11 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
 
         private void Btn_Close_Click(object sender, EventArgs e)
         {
-             
             this.Close();
         }
 
         private void btn_Locate_Click(object sender, EventArgs e)
         {
-
             OpenFileDialog fdlg = new OpenFileDialog();
             fdlg.Title = "Locate Game Root Directory";
             fdlg.InitialDirectory = @"c:\Program Files (x86)\";
@@ -317,55 +324,53 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
             if (fdlg.ShowDialog() == DialogResult.OK)
             {
                 string file = fdlg.FileName;
-  
+
                 file = file.Substring(0, file.Length - 10);
 
                 lbl_Location.Text = file;
                 setpath(file);
-               // storepathinmem();
+                // storepathinmem();
                 getusername();
                 checkgame();
-
             }
         }
 
         private void btn_Setusername_Click(object sender, EventArgs e)
         {
-
-                string un2 = File.ReadAllText(gameloc + "t6r\\player\\user.name");
-                setusername();
-
+            string un2 = File.ReadAllText(gameloc + "t6r\\player\\user.name");
+            setusername();
         }
 
-    
         private void btn_MP_Click(object sender, EventArgs e)
         {
-            if(usernameExists())
+            if (usernameExists())
             {
-                Process.Start(gameloc + "t6rmp.exe");
+                set_button_state(false);
+                var process = Process.Start(gameloc + "t6rmp.exe");
+                process.WaitForExit();
+                set_button_state(true);
             }
             else
             {
                 MessageBox.Show("Username Cannot Be Blank. Set a Username Before Launching the Game.", "Enter a Username.",
     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            
         }
 
         private void btn_ZM_Click(object sender, EventArgs e)
         {
             if (usernameExists())
             {
-
-                Process.Start(gameloc + "t6rzm.exe");
-
+                set_button_state(false);
+                var process = Process.Start(gameloc + "t6rzm.exe");
+                process.WaitForExit();
+                set_button_state(true);
             }
             else
             {
                 MessageBox.Show("Username Cannot Be Blank. Set a Username Before Launching the Game.", "Enter a Username.",
     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
 
         private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
@@ -382,9 +387,7 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
         {
             //help h = new help();
             //h.Show();
-           // Get_Latest_Version_Plutonium_Client();
+            // Get_Latest_Version_Plutonium_Client();
         }
-
-
     }
 }
--- Pluto Offline Launcher/Pluto Offline Launcher/Properties/AssemblyInfo.cs
+++ Pluto Offline Launcher/Pluto Offline Launcher/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pluto Offline Launcher")]
+[assembly: AssemblyTitle("Plutonium Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pluto Offline Launcher")]
+[assembly: AssemblyProduct("Plutonium Launcher")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.5.1")]
+[assembly: AssemblyFileVersion("1.0.5.1")]
 [assembly: NeutralResourcesLanguage("en")]
 
